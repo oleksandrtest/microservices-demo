43ee142ea737a436dc6d1722b2d153978aeb85c5
omelnyk <metahawk@i.au>
2023-02-02 16:51:26 +0200

    elastic-search deploy

--- elastic/elastic-index-client/src/main/java/com/microservices/demo/elastic/index/client/service/impl/TwitterElasticIndexClient.java
+++ elastic/elastic-index-client/src/main/java/com/microservices/demo/elastic/index/client/service/impl/TwitterElasticIndexClient.java
@@ -6,16 +6,16 @@ import com.microservices.demo.elastic.index.client.util.ElasticIndexUtil;
 import com.microservices.demo.elastic.model.index.impl.TwitterIndexModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.IndexedObjectInformation;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
-@ConditionalOnProperty(name = "elastic-config.is-repository", havingValue = "false")
 public class TwitterElasticIndexClient implements ElasticIndexClient<TwitterIndexModel> {
 
     private static final Logger LOG = LoggerFactory.getLogger(TwitterElasticIndexClient.class);
@@ -36,11 +36,11 @@ public class TwitterElasticIndexClient implements ElasticIndexClient<TwitterInde
 
     @Override
     public List<String> save(List<TwitterIndexModel> documents) {
-        List<IndexQuery> indexQueries = indexUtil.getIndexQueries(documents);
-        List<String> documentIds = elasticOperations.bulkIndex(
+        List<IndexQuery> indexQueries = elasticIndexUtil.getIndexQueries(documents);
+        List<String> documentIds = Collections.singletonList((elasticsearchOperations.bulkIndex(
                 indexQueries,
-                IndexCoordinates.of(configData.getIndexName())
-        );
+                IndexCoordinates.of(elasticConfigData.getIndexName())
+        )).toString());
         LOG.info("Documents indexed successfully with type: {} and ids: {}", TwitterIndexModel.class.getName(),
                 documentIds);
         return documentIds;
